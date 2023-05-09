8ee05cd569fa62df6944121e9c9397b1dd47ae91
Camila Cristina <camila.alves@nectarcrm.com.br>
2023-05-08 23:23:24 -0300

    estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { defaultTheme } from './styles/themes/default'
 import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global'
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
